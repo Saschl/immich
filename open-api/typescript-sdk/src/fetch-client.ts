87f263caf2dc538b18bb26fd91bd9e3b92483084
Saschl <noreply@saschl.com>
2025-11-20 18:36:45 +0100

    photon

--- open-api/typescript-sdk/src/fetch-client.ts
+++ open-api/typescript-sdk/src/fetch-client.ts
@@ -2623,6 +2623,7 @@ export type SystemConfigPasswordLoginDto = {
 export type SystemConfigReverseGeocodingDto = {
     /** Enabled */
     enabled: boolean;
+    photonUrl: string;
 };
 export type SystemConfigServerDto = {
     /** External domain */
--- server/src/config.ts
+++ server/src/config.ts
@@ -89,6 +89,7 @@ export type SystemConfig = {
   };
   reverseGeocoding: {
     enabled: boolean;
+    photonUrl: string;
   };
   metadata: {
     faces: {
@@ -280,6 +281,7 @@ export const defaults = Object.freeze<SystemConfig>({
   },
   reverseGeocoding: {
     enabled: true,
+    photonUrl: '',
   },
   metadata: {
     faces: {
